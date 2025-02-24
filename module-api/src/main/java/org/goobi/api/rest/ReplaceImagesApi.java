db41106a53de3088cf3f075335cc6337e0043de4
Dominick Leppich <dominick.leppich@intranda.com>
2025-02-24 09:43:31 +0100

    wip: Spark to WS migration

--- module-api/src/main/java/org/goobi/api/rest/ReplaceImagesApi.java
+++ module-api/src/main/java/org/goobi/api/rest/ReplaceImagesApi.java
@@ -1,80 +1,73 @@
-package de.intranda.goobi.plugins.replace_images;
+package org.goobi.api.rest;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.reflect.Type;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import de.intranda.goobi.plugins.replace_images.model.GoobiImage;
+import de.intranda.goobi.plugins.replace_images.model.ImageNature;
+import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.SwapException;
+import de.sub.goobi.persistence.managers.ProcessManager;
+import de.sub.goobi.persistence.managers.StepManager;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.configuration.SubnodeConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 import org.goobi.beans.Process;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import de.intranda.goobi.plugins.ReplaceImages;
-import de.intranda.goobi.plugins.replace_images.model.GoobiImage;
-import de.intranda.goobi.plugins.replace_images.model.ImageNature;
-import de.sub.goobi.config.ConfigPlugins;
-import de.sub.goobi.config.ConfigurationHelper;
-import de.sub.goobi.helper.exceptions.DAOException;
-import de.sub.goobi.helper.exceptions.SwapException;
-import de.sub.goobi.persistence.managers.ProcessManager;
-import de.sub.goobi.persistence.managers.StepManager;
-import lombok.extern.log4j.Log4j;
-import spark.Route;
-import ugh.dl.DigitalDocument;
-import ugh.dl.DocStruct;
-import ugh.dl.Fileformat;
-import ugh.dl.Metadata;
-import ugh.dl.MetadataType;
-import ugh.dl.Prefs;
+import ugh.dl.*;
+import ugh.exceptions.PreferencesException;
+import ugh.exceptions.ReadException;
+import ugh.exceptions.WriteException;
 import ugh.fileformats.mets.MetsMods;
 import ugh.fileformats.slimjson.SlimDigitalDocument;
 
-@Log4j
-public class Handlers {
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+@Log4j2
+@Path("/plugins/replaceimages")
+public class ReplaceImagesApi {
+    // TODO: Duplicate of `module-base` field
+    public static String TITLE = "intranda_step_replace-images";
+
     private static Gson gson = new Gson();
     private static Type arrayListType = TypeToken.getParameterized(ArrayList.class, GoobiImage.class).getType();
     private static XMLConfiguration pluginConfig;
 
-    public static Route prefsForProcess = (req, res) -> {
-        Process p = ProcessManager.getProcessById(Integer.parseInt(req.params("processid")));
-        DocStruct topDs = p.readMetadataFile().getDigitalDocument().getLogicalDocStruct();
-        if (topDs.getType().isAnchor()) {
-            topDs = topDs.getAllChildren().get(0);
-        }
-        String rulesetFile = ConfigurationHelper.getInstance().getRulesetFolder() + "/" + p.getRegelsatz().getDatei();
+//    public static Route prefsForProcess = (req, res) -> {
+//        Process p = ProcessManager.getProcessById(Integer.parseInt(req.params("processid")));
+//        DocStruct topDs = p.readMetadataFile().getDigitalDocument().getLogicalDocStruct();
+//        if (topDs.getType().isAnchor()) {
+//            topDs = topDs.getAllChildren().get(0);
+//        }
+//        String rulesetFile = ConfigurationHelper.getInstance().getRulesetFolder() + "/" + p.getRegelsatz().getDatei();
+//
+//        res.header("content-type", "application-xml");
+//        OutputStream os = res.raw().getOutputStream();
+//        Files.copy(Paths.get(rulesetFile), os);
+//        return "";
+//    };
 
-        res.header("content-type", "application-xml");
-        OutputStream os = res.raw().getOutputStream();
-        Files.copy(Paths.get(rulesetFile), os);
-        return "";
-    };
 
-    @SuppressWarnings("unchecked")
-    public static Route allImages = (req, res) -> {
-        Process p = ProcessManager.getProcessById(Integer.parseInt(req.params("processid")));
-        int stepId = Integer.parseInt(req.params("stepId"));
-        String stepTitle = StepManager.getStepById(stepId).getTitel();
+    @GET
+    @Path("/process/{processid}/{stepid}/images")
+    public List<GoobiImage> allImages(@PathParam("processid") int processid, @PathParam("stepid") int stepid) throws ReadException, SwapException, IOException, PreferencesException {
+        Process p = ProcessManager.getProcessById(processid);
+        String stepTitle = StepManager.getStepById(stepid).getTitel();
 
         SubnodeConfiguration config = getProjectAndStepConfig(p.getProjekt().getTitel(), stepTitle);
         List<String> imageNatureNames = Arrays.asList(config.getStringArray("imageFolder"));
         List<Map<String, String>> imageNatureBasenameToNameMap = imageNatureNames.stream()
                 .sorted()
                 .map(folder -> createBasenameToNameMap(p, folder))
-                .collect(Collectors.toList());
+                .toList();
 
         DocStruct physDs = p.readMetadataFile().getDigitalDocument().getPhysicalDocStruct();
 
@@ -117,14 +110,12 @@ public class Handlers {
             }
         }
 
-        //        XMLConfiguration conf = ConfigPlugins.getPluginConfig(title);
-        res.header("content-type", "application/json");
         return images;
-    };
+    }
 
     public static Map<String, String> createBasenameToNameMap(Process p, String folder) {
         Map<String, String> basenameToName = new HashMap<>();
-        Path imagesPath = null;
+        java.nio.file.Path imagesPath = null;
         try {
             imagesPath = Paths.get(p.getConfiguredImageFolder(folder));
         } catch (IOException | SwapException | DAOException e) {
@@ -133,8 +124,8 @@ public class Handlers {
         if (imagesPath == null) {
             return basenameToName;
         }
-        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(imagesPath)) {
-            for (Path imagePath : dirStream) {
+        try (DirectoryStream<java.nio.file.Path> dirStream = Files.newDirectoryStream(imagesPath)) {
+            for (java.nio.file.Path imagePath : dirStream) {
                 String imageName = imagePath.getFileName().toString();
                 String basename = imageName.substring(0, imageName.lastIndexOf('.'));
                 basenameToName.put(basename, imageName);
@@ -145,18 +136,20 @@ public class Handlers {
         return basenameToName;
     }
 
-    public static Route deleteImage = (req, res) -> {
-        Process p = ProcessManager.getProcessById(Integer.parseInt(req.params("processid")));
-        String folder = req.params("folder");
-        Path imageDir = Paths.get(p.getConfiguredImageFolder(folder));
-        Files.delete(resolveSanitized(imageDir, req.params("name")));
-        return "";
-    };
+    @DELETE
+    @Path("/process/{processid}/images/{folder}/{name}")
+    public Response deleteImage(@PathParam("processid") int processid, @PathParam("folder") String folder, @PathParam("name") String name) throws DAOException, SwapException, IOException {
+        Process p = ProcessManager.getProcessById(processid);
+        java.nio.file.Path imageDir = Paths.get(p.getConfiguredImageFolder(folder));
+        Files.delete(resolveSanitized(imageDir, name));
+        return Response.ok().build();
+    }
 
-    @SuppressWarnings("unchecked")
-    public static Route updateImages = (req, res) -> {
-        Process p = ProcessManager.getProcessById(Integer.parseInt(req.params("processid")));
-        List<GoobiImage> newimages = gson.fromJson(req.body(), arrayListType);
+    @PUT
+    @Path("/process/{processid}/images")
+    public Response updateImages(@PathParam("processid") int processid, String body) throws ReadException, SwapException, IOException, PreferencesException, WriteException {
+        Process p = ProcessManager.getProcessById(processid);
+        List<GoobiImage> newimages = gson.fromJson(body, arrayListType);
         Map<String, GoobiImage> idMap = new HashMap<>();
         for (GoobiImage im : newimages) {
             idMap.put(im.getId(), im);
@@ -184,23 +177,25 @@ public class Handlers {
             }
         }
         p.writeMetadataFile(ff);
-        return "";
-    };
+        return Response.ok().build();
+    }
 
-    public static Route saveMets = (req, res) -> {
+    @POST
+    @Path("/process/{processid}/saveMets")
+    public Response saveMets(@PathParam("processid") int processid, SlimDigitalDocument slimDD) throws WriteException, SwapException, IOException, PreferencesException {
         //        ObjectMapper mapper = new ObjectMapper();
-        SlimDigitalDocument slimDD = gson.fromJson(req.body(), SlimDigitalDocument.class);
+//        SlimDigitalDocument slimDD = gson.fromJson(req.body(), SlimDigitalDocument.class);
         DigitalDocument dd = slimDD.toDigitalDocument();
-        Process p = ProcessManager.getProcessById(Integer.parseInt(req.params("processid")));
+        Process p = ProcessManager.getProcessById(processid);
         Fileformat ff = new MetsMods();
         ff.setDigitalDocument(dd);
 
         p.writeMetadataFile(ff);
-        return "";
-    };
+        return Response.ok().build();
+    }
 
-    public static Path resolveSanitized(Path root, String toResolve) {
-        Path relative = root.resolve(toResolve).normalize();
+    public static java.nio.file.Path resolveSanitized(java.nio.file.Path root, String toResolve) {
+        java.nio.file.Path relative = root.resolve(toResolve).normalize();
         if (!relative.startsWith(root)) {
             throw new IllegalArgumentException("Path traverses");
         }
@@ -209,7 +204,7 @@ public class Handlers {
 
     public static SubnodeConfiguration getProjectAndStepConfig(String projectName, String stepTitle) {
         if (pluginConfig == null) {
-            pluginConfig = ConfigPlugins.getPluginConfig(ReplaceImages.TITLE);
+            pluginConfig = ConfigPlugins.getPluginConfig(TITLE);
             pluginConfig.setExpressionEngine(new XPathExpressionEngine());
             pluginConfig.setReloadingStrategy(new FileChangedReloadingStrategy());
         }
@@ -235,5 +230,4 @@ public class Handlers {
         }
         return projectAndStepConfig;
     }
-
 }
--- module-base/src/main/java/de/intranda/goobi/plugins/ReplaceImages.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/ReplaceImages.java
@@ -1,6 +1,5 @@
 package de.intranda.goobi.plugins;
 
-import de.intranda.goobi.plugins.replace_images.Routes;
 import lombok.Data;
 import lombok.extern.log4j.Log4j;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
@@ -9,16 +8,14 @@ import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.enums.StepReturnValue;
 import org.goobi.production.plugin.interfaces.IGuiPlugin;
-import org.goobi.production.plugin.interfaces.IRestPlugin;
 import org.goobi.production.plugin.interfaces.IStepPlugin;
-import spark.Service;
 
 import java.util.HashMap;
 
 @Data
 @PluginImplementation
 @Log4j
-public class ReplaceImages implements IRestPlugin, IGuiPlugin, IStepPlugin {
+public class ReplaceImages implements IGuiPlugin, IStepPlugin {
     private Step step;
     private String returnPath;
     public static String TITLE = "intranda_step_replace-images";
@@ -71,11 +68,6 @@ public class ReplaceImages implements IRestPlugin, IGuiPlugin, IStepPlugin {
     @Override
     public String[] getJsPaths() {
         return new String[] { "app.js" };
-    }
-
-    @Override
-    public void initRoutes(Service http) {
-        Routes.initRoutes(http);
     }
 
     @Override
